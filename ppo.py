6d2a5e393371d3f8ae3eeac66d72a1c6c8c7e302
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2025-11-01 23:22:05 +0900

    building RL experiments

--- ppo.py
+++ ppo.py
@@ -5,10 +5,15 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions import Normal, Independent
 
+from optimizer import AdamWC
+
 
 class PPOMemory:
 
-    def __init__(self, batch_size):
+    def __init__(
+        self,
+        batch_size: int,
+    ):
         self.states = []
         self.probs = []
         self.vals = []
@@ -56,9 +61,10 @@ class ActorNetwork(nn.Module):
 
     def __init__(
         self,
-        n_actions,
-        input_dims,
-        alpha,
+        n_actions: int,
+        input_dims: int,
+        alpha: float,
+        optimizer_name: str = "Adam",
         fc1_dims: int = 256,
         fc2_dims: int = 256,
         chkpt_dir: str = "tmp/ppo",
@@ -70,14 +76,20 @@ class ActorNetwork(nn.Module):
 
         self.body = nn.Sequential(
             nn.Linear(*input_dims, fc1_dims),
-            nn.ReLU(),
+            nn.tanh(),
             nn.Linear(fc1_dims, fc2_dims),
-            nn.ReLU(),
+            nn.tanh(),
         )
         self.mu_head = nn.Linear(fc2_dims, n_actions)
         self.logstd_head = nn.Linear(fc2_dims, n_actions)
 
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        if optimizer_name == "Adam":
+            self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        elif optimizer_name == "AdamWC":
+            self.optimizer = AdamWC(self.parameters(), lr=alpha)
+        else:
+            raise ValueError(f"No optimizer named {optimizer_name}\navailable optimizers are Adam and AdamWC")
+
         self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
         self.to(self.device)
 
@@ -101,7 +113,13 @@ class ActorNetwork(nn.Module):
 class CriticNetwork(nn.Module):
 
     def __init__(
-        self, input_dims, alpha, fc1_dims=256, fc2_dims=256, chkpt_dir="tmp/ppo"
+        self,
+        input_dims: int,
+        alpha: float,
+        optimizer_name: str = "Adam",
+        fc1_dims: int = 256,
+        fc2_dims: int = 256,
+        chkpt_dir: str = "tmp/ppo",
     ):
 
         super(CriticNetwork, self).__init__()
@@ -109,13 +127,19 @@ class CriticNetwork(nn.Module):
         self.checkpoint_file = os.path.join(chkpt_dir, "critic_torch_ppo")
         self.critic = nn.Sequential(
             nn.Linear(*input_dims, fc1_dims),
-            nn.ReLU(),
+            nn.tanh(),
             nn.Linear(fc1_dims, fc2_dims),
-            nn.ReLU(),
+            nn.tanh(),
             nn.Linear(fc2_dims, 1),
         )
 
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        if optimizer_name == "Adam":
+            self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        elif optimizer_name == "AdamWC":
+            self.optimizer = AdamWC(self.parameters(), lr=alpha)
+        else:
+            raise ValueError(f"No optimizer named {optimizer_name}\navailable optimizers are Adam and AdamWC")
+
         self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
         self.to(self.device)
 
@@ -135,23 +159,41 @@ class Agent:
 
     def __init__(
         self,
-        n_actions,
-        input_dims,
-        gamma=0.99,
-        alpha=0.0003,
-        gae_lambda=0.95,
-        policy_clip=0.2,
-        batch_size=64,
-        n_epochs=10,
+        n_actions: int,
+        input_dims: int,
+        optimizer_name: str,
+        gamma: float = 0.99,
+        alpha: float = 0.0003,
+        gae_lambda: float = 0.95,
+        policy_clip: float = 0.2,
+        critic_coef: float = 0.5,
+        max_grad_norm: float = 0.5,
+        batch_size: int = 64,
+        n_epochs: int = 10,
+        norm_advantage: bool = True,
+        clip_value_loss: bool = True,
     ):
 
         self.gamma = gamma
         self.policy_clip = policy_clip
         self.n_epochs = n_epochs
         self.gae_lambda = gae_lambda
+        self.norm_advantage = norm_advantage
+        self.clip_value_loss = clip_value_loss
+        self.critic_coef = critic_coef
+        self.max_grad_norm = max_grad_norm
 
-        self.actor = ActorNetwork(n_actions, input_dims, alpha)
-        self.critic = CriticNetwork(input_dims, alpha)
+        self.actor = ActorNetwork(
+            n_actions,
+            input_dims,
+            optimizer_name,
+            alpha
+        )
+        self.critic = CriticNetwork(
+            input_dims,
+            optimizer_name,
+            alpha
+        )
         self.memory = PPOMemory(batch_size)
 
     def remember(self, state, action, probs, vals, reward, done):
@@ -210,6 +252,8 @@ class Agent:
                     discount *= self.gamma * self.gae_lambda
                 advantages[t] = a_t
             advantages = T.tensor(advantages, dtype=T.float32, device=self.actor.device)
+            if self.norm_advantage:
+                advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
 
             values = values.detach().clone().requires_grad_(True).to(self.actor.device)
             for batch in batches:
@@ -225,7 +269,6 @@ class Agent:
 
                 dist = self.actor(states)
                 critic_value = self.critic(states).squeeze(-1)
-
                 critic_value = T.squeeze(critic_value)
 
                 new_logp = dist.log_prob(actions)
@@ -237,13 +280,31 @@ class Agent:
                 )
                 actor_loss = -T.min(surr1, surr2).mean()
 
+                # use the same clip with policy
+                value_clip = self.policy_clip
                 returns = advantages[batch] + values[batch]
-                critic_loss = (returns - critic_value).pow(2).mean()
+                if clip_value_loss:
+                    unclipped_critic_loss = (returns - critic_value).pow(2).mean()
+                    clipped_critic_value = values[batch] + T.clamp(
+                        critic_value - values[batch],
+                        -value_clip,
+                        value_clip,
+                    )
+                    clipped_critic_loss = (clipped_critic_value).pow(2).mean()
+                    max_critic_loss = T.max(
+                        unclipped_critic_loss,
+                        clipped_critic_loss,
+                    )
+                    critic_loss = 0.5 * max_critic_loss
+                else:
+                    critic_loss = 0.5 * (returns - critic_value).pow(2).mean()
 
-                total_loss = actor_loss + 0.5 * critic_loss
+                total_loss = actor_loss + self.critic_coef * critic_loss
                 self.actor.optimizer.zero_grad()
                 self.critic.optimizer.zero_grad()
                 total_loss.backward()
+                nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
+                nn.utils.clip_grad_norm_(self.critic.parameters(), self.max_grad_norm)
                 self.actor.optimizer.step()
                 self.critic.optimizer.step()
 
--- ppo_main.py
+++ ppo_main.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import gymnasium as gym
 import numpy as np
 from ppo import Agent
-from utils import plot_learning_curve
 from torch.utils.tensorboard import SummaryWriter
 
 writer = SummaryWriter()  # Tensorboard
@@ -13,12 +12,17 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gamma", type=float, default=0.99, help="discount")
     parser.add_argument("--alpha", type=float, default=0.0003, help="learning rate")
-    parser.add_argument("--gae_lambda", type=float, default=0.95, help="gae lambda")
-    parser.add_argument("--policy_clip", type=float, default=0.2, help="policy clip")
-    parser.add_argument("--batch_size", type=int, default=64, help="batch size")
+    parser.add_argument("--gae-lambda", type=float, default=0.95, help="gae lambda")
+    parser.add_argument("--policy-clip", type=float, default=0.2, help="policy clip")
+    parser.add_argument("--critic-coef", type=float, default=0.5)
+    parser.add_argument("--max-grad-norm", type=float, default=0.5)
+    parser.add_argument("--norm-advantage", type=bool, default=True)
+    parser.add_argument("--clip-value-loss", type=bool, default=True)
+    parser.add_argument("--batch-size", type=int, default=64, help="batch size")
     parser.add_argument("--epochs", type=int, default=10, help="epochs")
     parser.add_argument("--n", type=int, default=20, help="update frequency")
-    parser.add_argument("--optimizer", type=str)
+    parser.add_argument("--optimizer", type=str, default='Adam')
+    parser.add_argument("--total-timsteps", type=int, default=1_000_000)
 
     args = parser.parse_args()
 
@@ -29,23 +33,31 @@ if __name__ == "__main__":
 
     args = get_args()
 
-    env = gym.make("Humanoid-v5")
+    print(0)
+    env = gym.make("HalfCheetah-v5")
+    print(1)
     N = args.n
     batch_size = args.batch_size
     n_epochs = args.epochs
     alpha = args.alpha
+    num_iteration = args.total_timesteps // batch_size
 
     agent = Agent(
         n_actions=1,
         gamma=args.gamma,
+        optimizer_name=args.optimizer,
         alpha=args.alpha,
         gae_lambda=args.gae_lambda,
         policy_clip=args.policy_clip,
+        critic_coef=args.critic_coef,
+        max_grad_norm=args.max_grad_norm,
         batch_size=args.batch_size,
         n_epochs=args.epochs,
         input_dims=env.observation_space.shape,
+        norm_advantage=args.norm_advantage,
+        clip_value_loss=args.clip_value_loss,
     )
-    n_games = 300
+    print(3)
 
     best_score = env.reward_range[0]
     score_history = []
@@ -54,7 +66,7 @@ if __name__ == "__main__":
     avg_score = 0
     n_steps = 0
 
-    for i in range(n_games):
+    while n_steps < args.total_timesteps:
         observation = env.reset()
         done = False
         score = 0
@@ -65,6 +77,13 @@ if __name__ == "__main__":
             score += reward
             agent.remember(observation, action, prob, val, reward, done)
             if n_steps % N == 0:
+
+                # Annealing
+                frac = 1.0 - (learn_iters - 1.0) / num_iterations
+                lrnow = frac * args.alpha
+                agent.actor.optimizer.param_group[0]["lr"] = lrnow
+                agent.critic.optimizer.param_group[0]["lr"] = lrnow
+
                 agent.learn()
                 learn_iters += 1
             observation = observation_
@@ -87,4 +106,4 @@ if __name__ == "__main__":
         )
 
     x = [i + 1 for i in range(len(score_history))]
-    plot_learning_curve(x, score_history, figure_file)
+    # plot_learning_curve(x, score_history, figure_file)
